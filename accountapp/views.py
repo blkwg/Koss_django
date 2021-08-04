5d31f79a1a26176418d944d0396592c60ded7d77
blkwg <dodobary@naver.com>
2021-08-04 21:16:03 +0900

    25강: 업데이트뷰로 비밀번호 변경 구현

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,9 +5,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView
 
 from accountapp.models import Helloworld
+from accountapp.templates.forms import AccountUpdateForm
 
 
 def hello_world(request):
@@ -41,6 +42,14 @@ class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+
+
+class AccountUpdateView(UpdateView):
+    model = User
+    form_class = AccountUpdateForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/update.html'
+
 
 
 
