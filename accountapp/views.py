fe428990aef40fca4a91e7e2fbb867bed0331f26
blkwg <dodobary@naver.com>
2021-08-12 23:58:18 +0900

    28. decorator를 이용하여 코드 길이 줄이기(accounts로 리다이렉트 되는건 어떻게 해결하냐??)

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,35 +1,42 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from accountapp.decoraters import account_ownership_required
 from accountapp.models import Helloworld
 from accountapp.templates.forms import AccountUpdateForm
 
+has_ownership = [account_ownership_required, login_required]
 
+@login_required
 def hello_world(request):
 
-    if request.method == "POST":
-        temp = request.POST.get('hello_world_input')
+        if request.method == "POST":
+            temp = request.POST.get('hello_world_input')
 
-        new_hello_world = Helloworld()
-        new_hello_world.text = temp
-        new_hello_world.save()
+            new_hello_world = Helloworld()
+            new_hello_world.text = temp
+            new_hello_world.save()
 
-        hello_world_list = Helloworld.objects.all()
+            hello_world_list = Helloworld.objects.all()
 
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+            return HttpResponseRedirect(reverse('accountapp:hello_world'))
 
-    else:
-        hello_world_list = Helloworld.objects.all()
-        return render(request, 'accountapp/helloworld.html', context={'hello_world_output': hello_world_list})
+        else:
+            hello_world_list = Helloworld.objects.all()
+            return render(request, 'accountapp/helloworld.html', context={'hello_world_output': hello_world_list})
 
 
-#-------------------------------------------
+
+
+
 
 class AccountCreateView(CreateView):
     model = User
@@ -43,17 +50,25 @@ class AccountDetailView(DetailView):
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
-
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountUpdateForm
+    context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
+
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User
+    context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:login')
     template_name = 'accountapp/delete.html'
+
+
 
 
 
